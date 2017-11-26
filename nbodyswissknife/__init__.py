19924a58ab8c0a0b2df8c5db6d42ff75426c9a0c
Mher Kazandjian <mherkazandjian@gmail.com>
2017-11-26 09:51:12 +0100

    add boilderplate code for chosing a backend

---
+++ nbodyswissknife/__init__.py
@@ -0,0 +1,10 @@
+class Configuration(object):
+    def __init__(self):
+        self.backends = {
+
+        }
+
+config_params = Configuration()
+
+def use(backend):
+    pass
---
+++ nbodyswissknife/potential.py
@@ -0,0 +1,3 @@
+# from nbodyswissknife.backends.cpu import
+def potential_cpu(mass, x, y, z):
+    return phi
