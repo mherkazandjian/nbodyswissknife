a83f4d2d09b5065ff64ef2988abcd0b5065da3c1
Mher Kazandjian <mherkazandjian@gmail.com>
2017-12-08 19:00:15 +0100

    implement computing the potential energy of a distribution

--- examples/distribution_potential.py
+++ examples/distribution_potential.py
@@ -1,32 +1,35 @@
 """
 <keywords>
-test, python, potential, nbody
+test, python, potential, nbody, plummer
 </keywords>
 <description>
-example script for computing the potential for a bunch of particles
+example script for computing the potential for a sampled plummer sphere
 </description>
 <seealso>
 </seealso>
 """
 import numpy
-numpy.random.seed(1234)
 
 from astropy import units as u
 from astropy import constants as const
 
-from nbodyswissknife import potential_cpu
+from nbodyswissknife import potential
 
 #
-# sample a plummer sphere with core radius 'a' and mass 'M'
+# sample a plummer sphere with core radius 'b' and mass 'M'
 #
-n = 10
-a = 2.0 * u.pc
-M = 1000.0 * const.M_sun
+n = 1000                   # number of particles
+b = 5.3 * u.pc             # scale length of the plummer model
+M = 132.0 * const.M_sun    # the mass of the cluster
+G = const.G                # gravitational constant
+r_loc = numpy.array([1.0, 2.0, 3.0]) * u.pc
 
-
+#
+# sample a plummer sphere with core radius 'b' and mass 'M'
+#
 phi = numpy.random.rand(n)*2.0*numpy.pi
 theta = numpy.arccos((numpy.random.rand(n) - 0.5)*2.0)
-r = a / numpy.sqrt(numpy.random.rand(n)**(-2.0/3.0) - 1)
+r = b / numpy.sqrt(numpy.random.rand(n)**(-2.0/3.0) - 1)
 
 m = numpy.ones(n) * M / n
 x = r*numpy.sin(theta)*numpy.cos(phi)
@@ -36,13 +39,23 @@ z = r*numpy.cos(theta)
 #
 # compute the potential of the cluster at a certain distance
 #
-pot = potential_cpu.potential(
-    x.value,
-    y.value,
-    z.value,
-    m.value,
-    0.0,
-    const.G.value
+pot = potential.potential_native(
+    numpy.vstack((x, y, z)),
+    m,
+    soft=0.0,
+    gauss=G,
+    location=r_loc,
+)
+
+pot_theoretical_plummer_at_r_loc = - G * M / numpy.sqrt(
+    numpy.linalg.norm(r_loc)**2 + b.value**2
+)
+
+print('computed potential at r_loc    = ', pot)
+print('theoretical potential at r_loc = ', pot_theoretical_plummer_at_r_loc)
+print(
+    'relative difference          = ',
+    1.0 - (pot / pot_theoretical_plummer_at_r_loc).value
 )
 
 print(pot)
---
+++ examples/distribution_potential_energy.py
@@ -0,0 +1,58 @@
+"""
+<keywords>
+test, python, potential, energy, nbody
+</keywords>
+<description>
+example script for computing the potential energy of a sampled plummer sphere
+</description>
+<seealso>
+</seealso>
+"""
+import numpy
+
+from astropy import units as u
+from astropy import constants as const
+
+from nbodyswissknife import potential
+
+#
+# set the parameters of the model
+#
+n = 100                   # number of particles
+b = 5.3 * u.pc            # scale length of the plummer model
+M = 132.0 * const.M_sun   # the mass of the cluster
+G = const.G               # gravitational constant
+
+#
+# sample a plummer sphere with core radius 'b' and mass 'M'
+#
+phi = numpy.random.rand(n)*2.0*numpy.pi
+theta = numpy.arccos((numpy.random.rand(n) - 0.5)*2.0)
+r = b / numpy.sqrt(numpy.random.rand(n)**(-2.0/3.0) - 1)
+
+m = numpy.ones(n) * M / n
+x = r*numpy.sin(theta)*numpy.cos(phi)
+y = r*numpy.sin(theta)*numpy.sin(phi)
+z = r*numpy.cos(theta)
+
+#
+# compute the potential energy of the cluster at a certain distance
+#
+pot_energy = potential.potential_energy_native(
+    numpy.vstack((x, y, z)).value,
+    m.value,
+    soft=0.0,
+    gauss=G
+)
+
+pot_energy_theoretical_plummer = - 3.0*numpy.pi*G*M**2 / (32.0*b)
+
+print('computed potential energy    = ', pot_energy)
+print('theoretical potential energy = ', pot_energy_theoretical_plummer)
+print(
+    'relative difference          = ',
+    1.0 - (pot_energy / pot_energy_theoretical_plummer).value
+)
+
+print('done')
+
