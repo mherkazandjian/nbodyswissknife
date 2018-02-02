6b55393d7186b5aea9e01adde0d985fd64471e28
Mher Kazandjian <mherkazandjian@gmail.com>
2018-02-02 17:23:05 +0100

    add script that compared mesh energy computation to the theoritcal one

--- examples/distribution_potential_energy_grid.py
+++ examples/distribution_potential_energy_grid.py
@@ -37,7 +37,44 @@ x = r*numpy.sin(theta)*numpy.cos(phi)
 y = r*numpy.sin(theta)*numpy.sin(phi)
 z = r*numpy.cos(theta)
 
-raise NotImplementedError('continue from here')
+#
+# construct the mass grid (bin masses in the grid)
+#
+r_max = 20*b
+nbins = 50
+xgrid, ygrid, zgrid = numpy.meshgrid(
+    numpy.linspace(-r_max, r_max, nbins),
+    numpy.linspace(-r_max, r_max, nbins),
+    numpy.linspace(-r_max, r_max, nbins)
+)
+assert xgrid.shape == ygrid.shape == zgrid.shape
+grid_shape = xgrid.shape
+
+bsz = xgrid[0, 1, 0] - xgrid[0, 0, 0]  # all dims have equal bin sizes
+mass_grid = numpy.zeros_like(xgrid.value).flatten()
+mass_grid_cntrd_x = numpy.zeros_like(xgrid.value).flatten()
+mass_grid_cntrd_y = numpy.zeros_like(xgrid.value).flatten()
+mass_grid_cntrd_z = numpy.zeros_like(xgrid.value).flatten()
+
+for i, (_x, _y, _z) in enumerate(
+    zip(xgrid.flatten(), ygrid.flatten(), zgrid.flatten())):
+    inds = numpy.where(
+        (x >= _x) * (x < _x + bsz) *
+        (y >= _y) * (y < _y + bsz) *
+        (z >= _z) * (z < _z + bsz)
+    )
+    mass_grid[i] = m[inds].sum().value
+    mass_grid_cntrd_x[i] = (_x + bsz / 2.0).value
+    mass_grid_cntrd_y[i] = (_y + bsz / 2.0).value
+    mass_grid_cntrd_z[i] = (_z + bsz / 2.0).value
+    # print(i, int(i % (int(nbins**3) / 100)))
+    if (i % int((nbins**3) / 100)) == 0:
+        print('{:.2f}'.format((i / nbins**3)*100))
+
+mass_grid = mass_grid.reshape(xgrid.shape) * m.unit
+mass_grid_cntrd_x = mass_grid_cntrd_x.reshape(xgrid.shape) * x.unit
+mass_grid_cntrd_y = mass_grid_cntrd_y.reshape(xgrid.shape) * y.unit
+mass_grid_cntrd_z = mass_grid_cntrd_z.reshape(xgrid.shape) * z.unit
 
 #
 # compute the potential energy of the cluster at a certain distance
